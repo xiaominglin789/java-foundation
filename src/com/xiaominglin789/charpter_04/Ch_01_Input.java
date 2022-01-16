9568fc14f2f8f9203d779162c2cd1dc91766e1f5
xiaominglin789 <xiaominglin789@gmail.com>
2022-01-16 13:16:54 +0800

    进制转换

---
+++ src/com/xiaominglin789/charpter_04/Ch_01_Input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ch_01_Input {
+  public static void main(String[] args) {
+    // 实例Scanner对象
+    Scanner scanner = new Scanner(System.in);
+    // 接收输入
+    String inputValue = scanner.nextLine();
+    if (inputValue != null) {
+      System.out.println("你输入的是: " + inputValue);
+    }
+    
+  }
+}
