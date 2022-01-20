a259610d1879451d1036c91bfb021555c2a12aea
xiaominglin789 <xiaominglin789@gmail.com>
2022-01-20 23:59:51 +0800

    数组练习

--- src/com/xiaominglin789/charpter_07/Ch_2_Pratice.java
+++ src/com/xiaominglin789/charpter_07/Ch_2_Pratice.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Ch_2_Pratice {
   public static void main(String[] args) {
     /**
@@ -18,12 +20,79 @@ public class Ch_2_Pratice {
      a) String strs[] = {'a', 'b', 'c'}; // 'a'、'b'、'c 字符
      b) String[] strs = {"a", "b", "c"}; // 一维数组
      c) String[] strs = new String({"a", "b", "c"}); // 编译报错
-     d) String strs[] = new String[]{"a", "b", "c"}; // ok ?
+     d) String strs[] = new String[]{"a", "b", "c"}; // ok...
      e) String[] strs = new String[]{"a", "b", "c"}; // 一维数组 = 二维数组 ?
      */
     String strs[] = new String[]{"a", "b", "c"};
     for (String s : strs) {
       System.out.println(s + " ");
     }
+
+    /**
+    题目:
+     已知一个升序的数组, 要求插入一个元素，该数组顺序依然是升序的。
+      int[] upArr = {10,12,45,90};
+
+      1.查找位置
+      2.数组先扩容的同时,是插入对应的位置。
+     */
+    
+    int[] upArr = {10,12,45,90};
+    System.out.println("原数组: " + toString(upArr));
+
+    Scanner sc = new Scanner(System.in);
+    System.out.print("\n任意输入一个数字: ");
+    int insertNum = sc.nextInt();
+    int willInsertIndex = 0;
+    for (int i=0; i < upArr.length-1; i++) {
+      if (insertNum < upArr[i]) {
+        willInsertIndex = i;
+        break;
+      }
+    }
+    // 旧数组扩容操作
+    int[] upArrTemp = new int[upArr.length+1];
+    for(int i=0; i <= upArrTemp.length-1; i++) {
+      if (i < willInsertIndex) {
+        // i < x
+        upArrTemp[i] = upArr[i];
+      } else if (i == willInsertIndex){
+        // i == x
+        upArrTemp[willInsertIndex] = insertNum;
+      } else {
+        // i > x
+        // System.out.println(i);
+        upArrTemp[i] = upArr[i-1];
+      }
+    }
+    System.out.println("扩容后新数组: " + toString(upArrTemp));
+
+
+    /**
+    下列代码输出:
+    char[] arr1 = {'a', 'b', 'c', 'd'}
+    char[] arr2 = arr1; // 引用传递
+    arr1[2] = '中'; // 改值互相影响
+    for (int i=0; i < arr2.length; i++) {
+      System.out.println(arr1[i] + ", " + arr2[i])
+    }
+     */
+  }
+
+  /**
+  打印整型数组
+   */
+  public static String toString(int[] arr) {
+    String result = "{";
+
+    for (int i=0; i<arr.length; i++) {
+      if (i > 0 && i < arr.length) {
+        result += ", ";
+      }
+      result += arr[i];
+    }
+    result += "}";
+
+    return result;
   }
 }
