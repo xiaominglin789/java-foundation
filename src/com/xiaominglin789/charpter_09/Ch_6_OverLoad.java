f5a6103d47a7580584a9c01f680344d69610c63f
xiaominglin789 <xiaominglin789@gmail.com>
2022-01-24 13:25:55 +0800

    方法的重载

---
+++ src/com/xiaominglin789/charpter_09/Ch_6_OverLoad.java
@@ -0,0 +1,28 @@
+public class Ch_6_OverLoad {
+  public static void main(String[] args) {
+    OverLoadMethods m = new OverLoadMethods();
+    System.out.println("m.max(11, 32): " + m.max(11, 32));
+    System.out.println("m.max(0.8, 1): " + m.max(0.8, 1));
+    System.out.println("m.max(1.1, 1.0): " + m.max(1.1, 1.0));
+    System.out.println("m.max(6.6, 3.4, 7): " + m.max(6.6, 3.4, 7));
+  }
+}
+
+class OverLoadMethods {
+  public int max(int n1, int n2) {
+    return n1 > n2 ? n1 : n2;
+  }
+
+  public double max(double n1, double n2) {
+    return n1 > n2 ? n1 : n2;
+  }
+
+  public double max(double n1, int n2) {
+    // System.out.println("形参: double, int");
+    return n1 > n2 ? n1 : n2;
+  }
+
+  public double max(double n1, double n2, double n3) {
+    return n1 > n2 ? (n1 > n3 ? n1 : n3) : (n2 > n3 ? n2 : n3);
+  }
+}
